35f735a75b1d25deec288eb5c93af5171fcc6f3b
Phillip Reynolds <preynolds1406@gmail.com>
2022-10-08 10:45:30 -0500

    Reorganized board reducer to make it more readable.

--- src/redux/board/board.reducer.js
+++ src/redux/board/board.reducer.js
@@ -13,10 +13,98 @@ const INITIAL_STATE = generateInitialState();
 
 const boxReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        // actions related to resetting board
         case BoardActionTypes.RESET_BOARD:
             return {
                 ...INITIAL_STATE
             }
+        case BoardActionTypes.RESET_BOARD_TO_START:
+            return {
+                ...state,
+                logicRounds: 0,
+                checkForGivensRounds: 0,
+                checkForGivensArray: [],
+                slicingRounds: 0,
+                slicingArray: []
+            }
+
+        // actions related to error state of board
+        case BoardActionTypes.ADD_ERROR:
+            return {
+                ...state,
+                errors: addErrorToBoard(state.errors, action.error)
+            }
+        case BoardActionTypes.CLEAR_ERROR:
+            return {
+                ...state,
+                errors: removeErrorFromBoard(state.errors, action.boxId)
+            }
+        case BoardActionTypes.CLEAR_ALL_ERRORS:
+            return {
+                ...state,
+                errors: {}
+            }
+
+        // actions related to solving state of board
+        case BoardActionTypes.BOARD_START_SAVED:
+            return {
+                ...state,
+                solving: true
+            }
+        case BoardActionTypes.STOP_SOLVING:
+            return {
+                ...state,
+                solving: false
+            }
+
+        // actions related to board stats
+        case BoardActionTypes.INCREASE_INPUTTED_NUMBER:
+            return {
+                ...state,
+                boxesInputted: state.boxesInputted + 1
+            }
+        case BoardActionTypes.DECREASE_INPUTTED_NUMBER:
+            return {
+                ...state,
+                boxesInputted: state.boxesInputted - 1
+            }
+        case BoardActionTypes.INCREASE_SOLVED_NUMBER:
+            return {
+                ...state,
+                boxesSolved: state.boxesSolved + 1
+            }
+        case BoardActionTypes.DECREASE_SOLVED_NUMBER:
+            return {
+                ...state,
+                boxesSolved: state.boxesSolved - 1
+            }
+        case BoardActionTypes.INCREASE_LOGIC_ROUNDS:
+            return {
+                ...state,
+                logicRounds: state.logicRounds + 1
+            }
+        case BoardActionTypes.INCREASE_CHECK_FOR_GIVENS_ROUNDS:
+            return {
+                ...state,
+                checkForGivensRounds: state.checkForGivensRounds + 1
+            }
+        case BoardActionTypes.ADD_BOX_TO_CHECK_FOR_GIVENS_ARRAY:
+            return {
+                ...state,
+                checkForGivensArray: addBoxToCheckForGivensArray(state.checkForGivensArray, action.given)
+            }
+        case BoardActionTypes.INCREASE_SLICING_ROUNDS:
+            return {
+                ...state,
+                slicingRounds: state.slicingRounds + 1
+            }
+        case BoardActionTypes.ADD_BOX_TO_SLICED_ARRAY:
+            return {
+                ...state,
+                slicingArray: addBoxToSlicedArray(state.slicingArray, action.sliced)
+            }
+
+        // actions related to updating box state
         case BoardActionTypes.UPDATE_BOX_VALUE:
             return {
                 ...state,
@@ -28,16 +116,6 @@ const boxReducer = (state = INITIAL_STATE, action) => {
                     }
                 }
             }
-        case BoardActionTypes.ADD_ERROR:
-            return {
-                ...state,
-                errors: addErrorToBoard(state.errors, action.error)
-            }
-        case BoardActionTypes.CLEAR_ERROR:
-            return {
-                ...state,
-                errors: removeErrorFromBoard(state.errors, action.boxId)
-            }
         case BoardActionTypes.ADD_ERROR_TO_BOX:
             return {
                 ...state,
@@ -48,11 +126,6 @@ const boxReducer = (state = INITIAL_STATE, action) => {
                         hasError: true
                     }
                 }
-            }
-        case BoardActionTypes.CLEAR_ALL_ERRORS:
-            return {
-                ...state,
-                errors: {}
             }
         case BoardActionTypes.CLEAR_ALL_ERRORS_FROM_BOX:
             return {
@@ -121,16 +194,6 @@ const boxReducer = (state = INITIAL_STATE, action) => {
                     }
                 }
             }
-        case BoardActionTypes.BOARD_START_SAVED:
-            return {
-                ...state,
-                solving: true
-            }
-        case BoardActionTypes.STOP_SOLVING:
-            return {
-                ...state,
-                solving: false
-            }
         case BoardActionTypes.SAVE_BOX_AS_INPUTTED:
             return {
                 ...state,
@@ -154,58 +217,6 @@ const boxReducer = (state = INITIAL_STATE, action) => {
                     }
                 }
             }
-        case BoardActionTypes.INCREASE_INPUTTED_NUMBER:
-            return {
-                ...state,
-                boxesInputted: state.boxesInputted + 1
-            }
-        case BoardActionTypes.DECREASE_INPUTTED_NUMBER:
-            return {
-                ...state,
-                boxesInputted: state.boxesInputted - 1
-            }
-        case BoardActionTypes.INCREASE_SOLVED_NUMBER:
-            return {
-                ...state,
-                boxesSolved: state.boxesSolved + 1
-            }
-        case BoardActionTypes.DECREASE_SOLVED_NUMBER:
-            return {
-                ...state,
-                boxesSolved: state.boxesSolved - 1
-            }
-        case BoardActionTypes.RESET_BOARD_TO_START:
-            return {
-                ...state,
-                logicRounds: 0,
-                checkForGivensRounds: 0,
-                checkForGivensArray: []
-            }
-        case BoardActionTypes.INCREASE_LOGIC_ROUNDS:
-            return {
-                ...state,
-                logicRounds: state.logicRounds + 1
-            }
-        case BoardActionTypes.INCREASE_CHECK_FOR_GIVENS_ROUNDS:
-            return {
-                ...state,
-                checkForGivensRounds: state.checkForGivensRounds + 1
-            }
-        case BoardActionTypes.ADD_BOX_TO_CHECK_FOR_GIVENS_ARRAY:
-            return {
-                ...state,
-                checkForGivensArray: addBoxToCheckForGivensArray(state.checkForGivensArray, action.given)
-            }
-        case BoardActionTypes.INCREASE_SLICING_ROUNDS:
-            return {
-                ...state,
-                slicingRounds: state.slicingRounds + 1
-            }
-        case BoardActionTypes.ADD_BOX_TO_SLICED_ARRAY:
-            return {
-                ...state,
-                slicingArray: addBoxToSlicedArray(state.slicingArray, action.sliced)
-            }
         case BoardActionTypes.BOX_SOLVED:
             return {
                 ...state,
@@ -219,6 +230,8 @@ const boxReducer = (state = INITIAL_STATE, action) => {
                     }
                 }
             }
+
+        // default return
         default:
             return state;
     }
